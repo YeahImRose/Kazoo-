601b15b285ba27a9ceea05df3221128f737fc8d1
matthewknecht <infinitycreeper1@gmail.com>
2016-05-10 11:29:54 -0500

    Added in file for rework of the map system

---
+++ map.h
@@ -0,0 +1,32 @@
+/*
+ * map.h
+ *
+ *  Created on: May 9, 2016
+ *      Author: 1800887
+ */
+
+#ifndef MAP_H_
+#define MAP_H_
+#include <string>
+using namespace std;
+
+string cave = "You are in some cave, water is dripping from the ceiling.";
+string cave1 = "There is light coming from somewhere...";
+string cave2 = "The cave ends here.";
+
+string outside1 = "Above you, the sun hangs in the sky.";
+
+string forest1 = "The trees reach for the sky around you.";
+
+
+
+string icave = "A dripping sound comes from this direction.";
+string icave1 = "A faint amount of light reaches here.";
+string icave2 = "It seems the cave ends over here.";
+
+string ioutside1 = "Light streams in from the cave entrance.";
+
+string iforest1 = "The trees here are unimaginably tall.";
+
+
+#endif /* MAP_H_ */
